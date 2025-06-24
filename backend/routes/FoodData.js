8762475b4a4c84cfac801b33a2abb2c54e49bbfe
sadafshahab12 <sadafshahab123@yahoo.com>
2025-06-24 15:10:36 -0700

    chnage food data backend

--- backend/routes/FoodData.js
+++ backend/routes/FoodData.js
@@ -3,8 +3,12 @@ const foodDataRouter = express.Router();
 
 foodDataRouter.post("/food-data", (req, res) => {
   try {
-    // console.log(global.foodItems, global.foodCategory);
-    res.send([global.foodItems, global.foodCategory]);
+    const foodItems = global.foodItems || [];
+    const foodCategory = global.foodCategory || [];
+
+    console.log("Backend Data Being Sent:", foodItems, foodCategory);
+
+    res.send([foodItems, foodCategory]);
   } catch (error) {
     console.error(error.message);
     res.status(500).json({ message: "Internal Server Error" });
