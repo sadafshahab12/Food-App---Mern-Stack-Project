c7643806e8c0474051348776c1ff06f58edc50f5
sadafshahab12 <sadafshahab123@yahoo.com>
2025-06-24 14:48:28 -0700

    add index

--- frontend/src/components/FoodData.jsx
+++ frontend/src/components/FoodData.jsx
@@ -6,25 +6,18 @@ const FoodData = ({ searchTerm }) => {
   const [foodCategory, setFoodCategory] = useState([]);
 
   const fetchData = async () => {
-    try {
-      const response = await fetch(
-        `${import.meta.env.VITE_REACT_BACKEND_BASE_URL}/api/food-data`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      const foodData = await response.json();
-
-      setFoodItems(Array.isArray(foodData[0]) ? foodData[0] : []);
-      setFoodCategory(Array.isArray(foodData[1]) ? foodData[1] : []);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      setFoodItems([]);
-      setFoodCategory([]);
-    }
+    const response = await fetch(
+      `${import.meta.env.VITE_REACT_BACKEND_BASE_URL}/api/food-data`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const foodData = await response.json();
+    setFoodItems(foodData[0]);
+    setFoodCategory(foodData[1]);
   };
 
   // Load data on first render
@@ -34,14 +27,16 @@ const FoodData = ({ searchTerm }) => {
 
   return (
     <div>
-      {Array.isArray(foodCategory) && foodCategory.length > 0 ? (
-        foodCategory.map((category) => (
-          <div key={category._id || category.CategoryName}>
+      {foodCategory.map((category, index) => {
+        return (
+          <div
+            key={index} // unique key here
+          >
             <div className="text-xl font-bold cursor-pointer border-b border-slate-300 py-4 px-10 mx-10">
               {category.CategoryName}
             </div>
             <div className="my-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 justify-self-center px-6">
-              {Array.isArray(foodItems) && foodItems.length > 0 ? (
+              {foodItems.length > 0 ? (
                 foodItems
                   .filter(
                     (items) =>
@@ -58,10 +53,8 @@ const FoodData = ({ searchTerm }) => {
               )}
             </div>
           </div>
-        ))
-      ) : (
-        <div>There is no category</div>
-      )}
+        );
+      })}
     </div>
   );
 };
