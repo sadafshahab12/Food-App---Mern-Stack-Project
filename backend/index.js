09e0d15a9ed2efd144a2c4e9388b2a330dc97ef6
sadafshahab12 <sadafshahab123@yahoo.com>
2025-06-24 12:20:01 -0700

    done

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const mongoDBConnect = require("./db");
 const cors = require("cors");
-const helmet = require("helmet");
 const loginUserRouter = require("./routes/LoginUser");
 const createUserRouter = require("./routes/CreateUser");
 const foodDataRouter = require("./routes/FoodData");
@@ -16,13 +15,13 @@ app.use(
   cors({
     origin: process.env.FRONT_END_URL,
     methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type"],
   })
 );
-app.use(helmet);
+
 app.use(express.json());
 app.get("/", (req, res) => {
-  res.redirect(process.env.FRONT_END_URL);
+  res.send("Hello World");
 });
 app.use("/auth", createUserRouter);
 app.use("/auth", loginUserRouter);
@@ -30,5 +29,5 @@ app.use("/api", foodDataRouter);
 app.use("/api", orderDataRouter);
 app.use("/api", myOrderDataRouter);
 app.listen(port, () => {
-  console.log(`Server is running on ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
